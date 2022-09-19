86c8fd5e68fa768f8b0c12e13f8eda1cf49f6be4
Tahir <Tahir@TAHIR->
2022-09-19 22:10:08 +0300

    Redirected event listener.

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const board = document.querySelector('#board');
+const board = document.getElementById('board');
 const colors = ['#e74c3c', '#8c44ad', '#3498db', '#e67e22', '#2ecc71'];
 const SQUARES_NUMBER = 500;
 
 for(let i = 0; i < SQUARES_NUMBER; ++i) {
     const square = document.createElement('div');
     square.classList.add('square');
-
-    square.addEventListener('mouseover', onMouseOver);
-    square.addEventListener('mouseleave', onMouseLeave);
-
+    square.addEventListener('mouseleave', event => setColor(event.target, '#1d1d1d'));
     board.appendChild(square);
 }
 
+board.addEventListener('mouseover', onMouseOver);
 
-/**
- * @param {HTMLElement} element 
- * @param {string} color 
- */
 function setColor(element, color) {
     element.style.backgroundColor = color;
     element.style.boxShadow = `0 0 2px ${color}`;
@@ -28,11 +22,7 @@ function getRandomColor() {
 }
 
 function onMouseOver(event) {
-    const element = event.target;
-    setColor(element, getRandomColor());
-}
-
-function onMouseLeave(event) {
-    const element = event.target;
-    setColor(element, '#1d1d1d');
+    if (event.target.classList.contains('square')) {
+        setColor(event.target, getRandomColor());
+    }
 }
